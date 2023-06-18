04cac09f97250887b873ebcfeca0fe27f91ae745
Ghivalza Harviansyah <hrvnsyh.ghivalza@gmail.com>
2023-06-18 12:08:06 +0700

    create notification

--- app/src/main/java/com/d3if3020/smartmoney/MainActivity.kt
+++ app/src/main/java/com/d3if3020/smartmoney/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.d3if3020.smartmoney
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -7,6 +11,11 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val CHANNEL_ID = "updater"
+        const val PERMISSION_REQUEST_CODE = 1
+    }
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +23,17 @@ class MainActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+            manager?.createNotificationChannel(channel)
+        }
 
     }
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/d3if3020/smartmoney/view/BankListFragment.kt
+++ app/src/main/java/com/d3if3020/smartmoney/view/BankListFragment.kt
@@ -1,14 +1,21 @@
 package com.d3if3020.smartmoney.view
 
 import BankListAdapter
+import android.Manifest
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.d3if3020.smartmoney.MainActivity
 import com.d3if3020.smartmoney.databinding.FragmentBankListBinding
 import com.d3if3020.smartmoney.network.ApiStatus
 import com.d3if3020.smartmoney.viewmodel.BankListViewModel
@@ -61,6 +68,10 @@ class BankListFragment : Fragment() {
             ApiStatus.SUCCESS -> {
                 binding.progressBar.visibility = View.GONE
                 binding.networkError.visibility = View.GONE
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestNotificationPermission()
+                }
+
             }
             ApiStatus.FAILED -> {
                 binding.progressBar.visibility = View.GONE
@@ -68,4 +79,20 @@ class BankListFragment : Fragment() {
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
 }
